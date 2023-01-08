fa2ea89ae47b17c876cac22c31de85070cd770f1
paolocalzone <paolocalzone@protonmail.com>
2023-01-08 22:11:44 +0100

    fix: add types to Header

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,34 @@
-import "../styles/globals.css";
+import { useEffect, useRef } from "react";
+
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+
 import type { AppProps } from "next/app";
-import { Inter, Syne } from "@next/font/google";
+import "@/styles/tailwind.css";
+import "focus-visible";
+import { Syne } from "@next/font/google";
 
 const syne = Syne({
   subsets: ["latin"],
   variable: "--font-syne",
 });
-export default function App({ Component, pageProps }: AppProps) {
+
+function usePrevious(value: any) {
+  let ref = useRef();
+
+  useEffect(() => {
+    if (ref.current) {
+      ref.current = value;
+    }
+  }, [value]);
+
+  return ref.current;
+}
+export default function App({ Component, pageProps, router }: AppProps) {
+  let previousPathname = usePrevious(router.pathname);
   return (
     <main className={`${syne.variable} font-sans`}>
-      <Component {...pageProps} />
+      <Component previousPathname={previousPathname} {...pageProps} />
     </main>
   );
 }
