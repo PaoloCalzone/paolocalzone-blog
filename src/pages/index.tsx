288c6bae64c9b70e4a8eadf7e55eec27abd6cf80
paolocalzone <paolocalzone@protonmail.com>
2023-01-28 21:47:31 +0100

    Edit content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,24 +18,21 @@ export default function Home({}) {
   return (
     <>
       <Head>
-        <title>
-          Spencer Sharp - Software designer, founder, and amateur astronaut
-        </title>
+        <title>Paolo Calzone - Decentralized App builder</title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I’m Paolo, a decentralized App builder. Focused on bringing blockchain technology to real world use cases."
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            gm I&apos;m Paolo, a web3 developer
+            gm I&apos;m Paolo👋, a builder of decentralized Apps focused on
+            using blockchain tech to solve real world use cases.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Paolo, a decentralized App builder. Focused on bringing
+            blockchain technology to real world use cases.
           </p>
           <Link href="/blog/fr/premier-post">/blog/fr/premier-post</Link>
           <div className="mt-6 flex gap-6">
